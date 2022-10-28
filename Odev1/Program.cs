96c8efbe1a5c70e0725c92a2a96c096181b74d87
Beytullah Öztürk <ozbeytullah2@outlook.com>
2022-10-28 14:32:22 +0300

    Add project files.

---
+++ Odev1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0, sum = 0;
+            Console.Write("Bir sayı giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= sayi; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"{sayi}'a kadar olan sayıların toplamı: {sum}");
+        }
+    }
+}
